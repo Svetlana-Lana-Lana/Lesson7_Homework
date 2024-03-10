8f07cadc558d2088a316e1588028d59e09693ac0
Svetlana <Svetlanat1005@yandex.ru>
2024-03-10 17:49:09 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N (использовать рекурсию)
+
+void AllNaturalNumbers(int first, int last)
+{
+    if (first > last)
+    {
+        return;
+    }
+    Console.Write(first + " ");
+    AllNaturalNumbers(first + 1, last);
+    
+
+}
+
+Console.WriteLine("Enter first number ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter last number ");
+int last = Convert.ToInt32(Console.ReadLine());
+
+AllNaturalNumbers(first, last);
